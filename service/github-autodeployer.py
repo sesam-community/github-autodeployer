64c2ccd4df9fd3f9bdd8198b7374fb873a4a2be5
Erik <erik.leven@sesam.io>
2020-06-16 11:43:53 +0200

    added support for orchestrator and updated readme

--- service/github-autodeployer.py
+++ service/github-autodeployer.py
@@ -30,6 +30,7 @@ var_file_path: str = os.environ.get('VARIABLES_FILE_PATH')
 vault_git_token = os.environ.get('VAULT_GIT_TOKEN')
 vault_mounting_point = os.environ.get('VAULT_MOUNTING_POINT')
 vault_url = os.environ.get('VAULT_URL')
+orchestrator = os.environ.get('ORCHESTRATOR', False)
 
 git_username = os.environ.get('GIT_USERNAME', None)  # Needed if using clone_git_repov3
 
@@ -285,6 +286,36 @@ def check_for_unknown():
         logging.warning("else, prepare for unexpected behaviour. Hic Sunt Leones. You have been warned.")
         logging.warning("\n")
 
+def check_and_replace_orchestrator_pipes():
+    for old_filename in os.listdir(sesam_checkout_dir + "/unpacked/pipes/"):
+        with open(os.path.join(sesam_checkout_dir + "/unpacked/pipes/", old_filename), 'r') as f: # open in readonly mode
+            old_file = json.loads(f.read())
+            try:
+                old_file["metadata"]["orchestrator"]["original_configuration"]
+                for new_filename in os.listdir(git_cloned_dir + "/sesam-node/pipes/"):
+                    with open(os.path.join(git_cloned_dir + "/sesam-node/pipes/", new_filename), 'r') as g: # open in readonly mode
+                        new_file = json.loads(g.read())
+                        if old_file["metadata"]["orchestrator"]["original_configuration"] == new_file:
+                            logging.info("The pipe %s is restored to orchestrator mode" % new_file["_id"])
+                            with open(os.path.join(payload_dir + "/pipes/", new_filename), 'w') as h:
+                                h.write(json.dumps(old_file))
+            except KeyError:
+                None
+def check_and_replace_orchestrator_systems():
+    for old_filename in os.listdir(sesam_checkout_dir + "/unpacked/systems/"):
+        with open(os.path.join(sesam_checkout_dir + "/unpacked/systems/", old_filename), 'r') as f: # open in readonly mode
+            old_file = json.loads(f.read())
+            try:
+                old_file["metadata"]["orchestrator"]["original_configuration"]
+                for new_filename in os.listdir(git_cloned_dir + "/sesam-node/systems/"):
+                    with open(os.path.join(git_cloned_dir + "/sesam-node/systems/", new_filename), 'r') as g: # open in readonly mode
+                        new_file = json.loads(g.read())
+                        if old_file["metadata"]["orchestrator"]["original_configuration"] == new_file:
+                            logging.info("The system %s is restored to orchestrator mode" % new_file["_id"])
+                            with open(os.path.join(payload_dir + "/systems/", new_filename), 'w') as h:
+                                h.write(json.dumps(old_file))
+            except KeyError:
+                None
 
 if __name__ == '__main__':
     os.chdir("/service")
@@ -322,6 +353,9 @@ if __name__ == '__main__':
                     logging.error('Failed to upload variables to node!')
             elif upload_variables and variables is None:
                 logging.error('Upload variables is true but could not get variables to upload!')
+        if orchestrator:
+            check_and_replace_orchestrator_pipes()
+            check_and_replace_orchestrator_systems()
         logging.info(f"Uploading new configuration from github to node {sesam_api}")
         zip_payload()
         upload_payload()
